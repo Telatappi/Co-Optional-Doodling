92ea4967a66819a4dbc88fb1ed8dba4b0c857a42
Aleksi Luomala <Aleksi_L_93@hotmail.com>
2018-01-16 22:00:52 +0200

    Items can add negative stat buffs

--- Co-Optional Doodling/Co-Optional Doodling/Item.cpp
+++ Co-Optional Doodling/Co-Optional Doodling/Item.cpp
@@ -21,32 +21,32 @@ void Item::SetName(std::string _name)
 	m_name = _name;
 }
 
-void Item::SetHp(unsigned int _hp)
+void Item::SetHp(int _hp)
 {
 	m_hp = _hp;
 }
 
-void Item::SetAgility(unsigned int _agility)
+void Item::SetAgility(int _agility)
 {
 	m_agility = _agility;
 }
 
-void Item::SetLuck(unsigned int _luck)
+void Item::SetLuck(int _luck)
 {
 	m_luck = _luck;
 }
 
-void Item::SetWisdom(unsigned int _wisdom)
+void Item::SetWisdom(int _wisdom)
 {
 	m_wisdom = _wisdom;
 }
 
-void Item::SetAccuracy(unsigned int _accuracy)
+void Item::SetAccuracy(int _accuracy)
 {
 	m_accuracy = _accuracy;
 }
 
-void Item::SetAttack(unsigned int _attack)
+void Item::SetAttack(int _attack)
 {
 	m_attack = _attack;
 }
@@ -61,32 +61,32 @@ std::string Item::GetName()
 	return m_name;
 }
 
-unsigned int Item::GetHp()
+int Item::GetHp()
 {
 	return m_hp;
 }
 
-unsigned int Item::GetAgility()
+int Item::GetAgility()
 {
 	return m_agility;
 }
 
-unsigned int Item::GetLuck()
+int Item::GetLuck()
 {
 	return m_luck;
 }
 
-unsigned int Item::GetWisdom()
+int Item::GetWisdom()
 {
 	return m_wisdom;
 }
 
-unsigned int Item::GetAccuracy()
+int Item::GetAccuracy()
 {
 	return m_accuracy;
 }
 
-unsigned int Item::GetAttack()
+int Item::GetAttack()
 {
 	return m_attack;
 }
--- Co-Optional Doodling/Co-Optional Doodling/Item.h
+++ Co-Optional Doodling/Co-Optional Doodling/Item.h
@@ -9,30 +9,30 @@ public:
 	Item();
 	~Item();
 	void SetName(std::string name);
-	void SetHp(unsigned int hp);
-	void SetAgility(unsigned int agility);
-	void SetLuck(unsigned int luck);
-	void SetWisdom(unsigned int wisdom);
-	void SetAccuracy(unsigned int accuracy);
-	void SetAttack(unsigned int attack);
+	void SetHp(int hp);
+	void SetAgility(int agility);
+	void SetLuck(int luck);
+	void SetWisdom(int wisdom);
+	void SetAccuracy(int accuracy);
+	void SetAttack(int attack);
 	void SetType(std::string type);
 	
 	std::string GetName();
-	unsigned int GetHp();
-	unsigned int GetAgility();
-	unsigned int GetLuck();
-	unsigned int GetWisdom();
-	unsigned int GetAccuracy();
-	unsigned int GetAttack();
+	int GetHp();
+	int GetAgility();
+	int GetLuck();
+	int GetWisdom();
+	int GetAccuracy();
+	int GetAttack();
 	std::string GetType();
 private:
 	std::string m_name;
-	unsigned int m_hp;
-	unsigned int m_agility;
-	unsigned int m_luck;
-	unsigned int m_wisdom;
-	unsigned int m_accuracy;
-	unsigned int m_attack;
+	int m_hp;
+	int m_agility;
+	int m_luck;
+	int m_wisdom;
+	int m_accuracy;
+	int m_attack;
 	std::string m_type;
 };
 #endif
